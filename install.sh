72e454a7d808b55f9c3bc42044db39c12daa9af9
max <max22336733@gmail.com>
2022-02-09 20:43:12 +0800

    Add keymap config file

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 if [ ! -d "$HOME/.moc" ]
 then
     echo $HOME/.moc directory does not exist.
-    echo You need to install MOC first!
+    echo You need to install MOC and launch it at least once!
     exit
 fi
 
 chmod g-w ./moc_config/config
 cp -i ./moc_config/config $HOME/.moc/config
+cp -i ./moc_config/keymap $HOME/.moc/keymap
 
 if [ ! -d "$HOME/.moc/themes" ]
 then
