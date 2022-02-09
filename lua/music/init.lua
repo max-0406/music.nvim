9ec248bfac3c3f9a11ab2c44b7219d94b010822d
max <max22336733@gmail.com>
2022-02-09 15:19:08 +0800

    Add moc_config

--- lua/music/init.lua
+++ lua/music/init.lua
@@ -1,6 +1,8 @@
 local api = vim.api
-local buf, win
+local buf
 local window_id
+local music_dir = os.getenv("HOME") .. "/Music"
+
 local M = {}
 
 M.test = function()
@@ -8,13 +10,23 @@ M.test = function()
 end
 
 M.open_moc = function()
+    local set_config = " -C ./moc_config/config"
+    local set_theme = " -T ./moc_config/music.nvim_theme"
+    local set_music_dir = " -O 'MusicDir=\"" .. music_dir .. "\"'"
+    api.nvim_exec("terminal mocp" .. set_config .. set_theme .. set_music_dir, false)
+    api.nvim_exec("normal i", false)
+    api.nvim_buf_set_option(0, "bufhidden", "wipe")
+end
+
+-- open floating window
+M.open_fw = function()
     buf = api.nvim_create_buf(false, true)
 
     local max_width = api.nvim_get_option("columns")
     local max_height = api.nvim_get_option("lines")
 
-    local win_width = math.ceil(max_width * 0.6)
-    local win_height = math.ceil(max_height * 0.6)
+    local win_width = math.ceil(max_width * 0.7)
+    local win_height = math.ceil(max_height * 0.7)
 
     -- set the option of floating window
     local opts = {
@@ -27,13 +39,12 @@ M.open_moc = function()
 	border = "rounded"
     }
 
-    win = api.nvim_open_win(buf, true, opts)
-    api.nvim_exec("terminal mocp", false)
-    api.nvim_exec("normal i", false)
-    api.nvim_buf_set_option(0, "bufhidden", "wipe")
+    api.nvim_open_win(buf, true, opts)
+    M.open_moc()
 end
 
-M.close_moc = function()
+-- close floating window
+M.close_fw = function()
     api.nvim_buf_delete(0, {force = true})
 end
 
--- plugin/music.vim
+++ plugin/music.vim
@@ -1,4 +1,21 @@
+if exists("g:loaded_music") | finish | endif
+
+if !has("nvim-0.5")
+    echohl Error
+    echomsg "music.nvim is only available for Neovim versions 0.5 and above"
+    echohl clear
+    finish
+endif
+
+let s:save_cpo = &cpo
+set cpo&vim
+
 lua music = require("music")
 
-nnoremap <Leader>o :lua music.open_moc()<CR>
-tnoremap <Leader>c <C-\><C-n>:lua music.close_moc()<CR>
+nnoremap <Leader>o :lua music.open_fw()<CR>
+tnoremap <Leader>c <C-\><C-n>:lua music.close_fw()<CR>
+
+let g:loaded_music = 1
+
+let &cpo = s:save_cpo
+unlet s:save_cpo
